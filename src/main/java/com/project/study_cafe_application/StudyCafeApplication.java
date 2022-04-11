8aeca12d34659f5433b15affed5aaeacddba5d30
LAPTOP-N91UPOS7\dudtl <youngsik0266@gmail.com>
2022-04-11 21:47:43 +0900

    Initial commit

---
+++ src/main/java/com/project/study_cafe_application/StudyCafeApplication.java
@@ -0,0 +1,13 @@
+package com.project.study_cafe_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyCafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyCafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/study_cafe_application/StudyCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.study_cafe_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyCafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
